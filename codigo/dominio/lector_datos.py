6ea9606f275934e652b4cb988e2f30a67bfa23b5
fxbiid <fabidiaz2804@gmail.com>
2026-05-10 21:17:44 -0400

    Feat: agrega metodos de lectura para categorias y nombres del dataset wiki-topcats

--- codigo/dominio/lector_datos.py
+++ codigo/dominio/lector_datos.py
@@ -1,9 +1,9 @@
 class LectorArch:
     @staticmethod
-    def leer_mtx_filtrado(ruta, limite=10000):
+    def leer_mtx_filtrado(nom, limite=10000):
         enlaces = []
 
-        with open(ruta, "r", encoding="utf-8") as archivo:
+        with open(nom, "r", encoding="utf-8") as archivo:
             for linea in archivo:
                 if linea.startswith("%"):
                     continue
@@ -21,3 +21,54 @@ class LectorArch:
                         break
 
         return enlaces
+
+    @staticmethod
+    def leer_mtx_categorias(nom, limite=10000):
+        relaciones = []
+        with open(nom, "r", encoding="utf-8") as archivo:
+            for linea in archivo:
+                if linea.startswith("%"):
+                    continue
+
+                partes = linea.strip().split()
+
+                if len(partes) == 3:
+                    continue
+
+                if len(partes) == 2:
+                    id_articulo, id_categoria = map(int, partes)
+                    relaciones.append((id_articulo, id_categoria))
+
+                    if len(relaciones) >= limite:
+                        break
+        return relaciones
+
+    @staticmethod
+    def leer_nombres_articulos(nom):
+        nombres_articulos = []
+
+        with open(nom, "r", encoding="utf-8") as archivo:
+
+            for linea in archivo:
+
+                nombre = linea.strip()
+
+                if nombre:
+                    nombres_articulos.append(nombre)
+
+        return nombres_articulos
+
+    @staticmethod
+    def leer_nombres_categorias(nom):
+        nombres_categorias = []
+
+        with open(nom, "r", encoding="utf-8") as archivo:
+
+            for linea in archivo:
+
+                nombre = linea.strip()
+
+                if nombre:
+                    nombres_categorias.append(nombre)
+
+        return nombres_categorias
